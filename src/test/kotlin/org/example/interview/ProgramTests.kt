e00e53ac62981082719f5f0526c1316230a8c9b6
Ivo Huntjens <gamerkeeperson+github@gmail.com>
2023-07-06 15:07:10 +0200

    Add test cases

---
+++ src/test/kotlin/org/example/interview/ProgramTests.kt
@@ -0,0 +1,20 @@
+package org.example.interview
+
+import org.junit.jupiter.api.Test
+
+class ProgramTests {
+    @Test
+    fun `Returns Status#Active when PIN terminal has been successfully activated`() {
+        TODO()
+    }
+
+    @Test
+    fun `Returns Status#Inactive when PIN Terminal could not be found`() {
+        TODO()
+    }
+
+    @Test
+    fun `Returns Status#Inactive when PIN terminal is already attached to a different customer ID`() {
+        TODO()
+    }
+}
